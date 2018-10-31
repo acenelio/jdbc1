40d5c3786bb41902feb756815af250b4e5f63a7a
Nelio Alves <acenelio@gmail.com>
2018-10-31 11:36:51 -0300

    Initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		DB.closeConnection();
+	}
+}
---
+++ src/db/DB.java
@@ -0,0 +1,48 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+	private static Connection conn = null;
+	
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		}
+		catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+}
